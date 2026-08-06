a1d11bfc2bee4fd20e4a497ff2b7d61c566495ca
Rouzib <npayot@gmail.com>
2026-08-06 18:06:26 -0400

    Remove obsolete multi-GPU test placeholders.

--- tests/test_cuda_routing_2gpu.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-pytestmark = pytest.mark.gpu2
-
-
-@pytest.mark.skip(reason="requires two visible CUDA devices; covered by the benchmark worker on GPU hosts")
-def test_two_gpu_cuda_bidir_route():
-    pass
--- tests/test_local_pair_multigpu.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-pytestmark = pytest.mark.gpu2
-
-
-@pytest.mark.skip(reason="requires two visible CUDA devices")
-def test_fused_halo_matches_two_gpu_force():
-    pass
